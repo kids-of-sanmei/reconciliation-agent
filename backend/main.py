ffa1f3fbd9e71fd7c91c79b593ba0c521fc9daad
philister <15068775933@163.com>
2026-09-13 10:48:16 +0800

    创建引擎

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from app.api.router import router as api_router
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 
+from app.model.base_model import Base
+from app.model.user import User
 app = FastAPI()
 app.include_router(api_router)
 
@@ -11,7 +14,21 @@ async def add_process_time_header(request, call_next) -> None:
     print("结束")
     return response
 
+engine_url = "mysql+aiomysql://root:123456@127.0.0.1:3306/app?charset=utf8"
+async_demo = create_async_engine(
+    url= engine_url,
+    echo=True, # 输出日志
+    pool_size=10, #连接池活跃数量
+    max_overflow=20 #最大允许额外连接数量
+)
 
+async def creat_tables():
+    async with async_demo.begin() as connection:
+        await connection.run_sync(Base.metadata.create_all)
+
+@app.on_event("startup")
+async def start():
+    await creat_tables()
 @app.get("/") 
 def read_root():
     return {"message": "Hello World"}
